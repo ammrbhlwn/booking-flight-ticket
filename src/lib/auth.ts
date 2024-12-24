a4eb671c984403f48dbc5fc11554894f26e667e0
ammrbhlwn <muammar.bahalwan@gmail.com>
2024-12-25 02:10:30 +0700

    feat: setup erd and lucia auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,65 @@
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import prisma from '../../lib/prisma';
+import { Lucia } from 'lucia';
+import { RoleUser } from '@prisma/client';
+import { cookies } from 'next/headers';
+import { cache } from 'react';
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      name: attributes.name,
+      email: attributes.email,
+      role: attributes.role,
+      passport: attributes.passport,
+    };
+  },
+});
+
+export const getUser = cache(async () => {
+  const sessionId =
+    (await cookies()).get(lucia.sessionCookieName)?.value ?? null;
+  if (!sessionId) return null;
+
+  const { user, session } = await lucia.validateSession(sessionId);
+
+  try {
+    if (session && session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      (await cookies()).set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    if (!session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      (await cookies()).set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+  } catch {}
+  return user;
+});
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: {
+      name: string;
+      email: string;
+      role: RoleUser;
+      passport: string | null;
+    };
+  }
+}
