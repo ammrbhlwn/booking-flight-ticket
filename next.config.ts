5f9afa0083844aed78ed1c32228271b70a133e6b
ammrbhlwn <muammar.bahalwan@gmail.com>
2024-12-30 00:52:43 +0700

    feat: integrate data airplanes

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'grynlzhmkoblsgifpdum.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
