d9bd0586fbf217faced56ccea3c0285ea9e4ad5a
ammrbhlwn <muammar.bahalwan@gmail.com>
2025-01-01 17:38:54 +0700

    feat: slicing choose seat page

---
+++ src/app/(home)/choose-seat/[id]/components/seat-list.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import SeatItem from './seat-item';
+
+export default function SeatList() {
+  return (
+    <form className="flex flex-row justify-between gap-5">
+      <div className="flex gap-5">
+        <div className="flex flex-col gap-[19px]">
+          <SeatItem />
+          <SeatItem />
+        </div>
+        <div className="flex flex-col gap-[19px]">
+          <SeatItem />
+          <SeatItem />
+        </div>
+      </div>
+      <div className="flex gap-5">
+        <div className="flex flex-col gap-[19px]">
+          <SeatItem />
+          <SeatItem />
+        </div>
+        <div className="flex flex-col gap-[19px]">
+          <SeatItem />
+          <SeatItem />
+        </div>
+      </div>
+    </form>
+  );
+}
